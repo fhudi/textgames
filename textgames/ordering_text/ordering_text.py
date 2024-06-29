9ac5dc475251d1c61113c1c34e82459599426c5f
Frederikus Hudi <frederikus.hudi@gmail.com>
2024-06-29 23:43:05 +0900

    adding prompt generator for ordering game

--- textgames/ordering_text/ordering_text.py
+++ textgames/ordering_text/ordering_text.py
@@ -55,30 +55,37 @@ infix
 
 
 #%%
-class Rule:
-    def __init__(self):
-        pass
+class Scoring:
+    def __init__(self, point: int):
+        self.point = point
+        self.str_point_patterns = [
+            f"{{pattern}} gets {self.point} point{'s' if self.point > 1 else ''}",
+            f"add {self.point} point{'s' if self.point > 1 else ''} if {{pattern}}",
+        ]
 
-    def calc_point(self, word):
-        pass
+    def calc_score(self, word: str) -> int:
+        raise NotImplementedError()
 
-    def generate_rule(self):
-        pass
+    # def generate_pattern(self):
+    #     raise NotImplementedError()
 
     def generate_prompt(self):
-        pass
+        raise NotImplementedError()
+
+    def point_wrapper(self, pattern: str, randint: int = 0) -> str:
+        return self.str_point_patterns[randint].format(pattern=pattern)
 
 
 #%%
-class ConsecutiveRule(Rule):
+class ConsecutiveScoring(Scoring):
     regex_pattern = {
         'c': "[^aeiou]",
         'v': "[aeiou]",
     }
 
     def __init__(self, point=1, seq="cc"):
-        super().__init__()
-        _pattern = ""
+        super().__init__(point)
+        pattern = ""
         n = 1
         for a, b in zip(seq, seq[1:] + '$'):
             if a == b:
@@ -87,26 +94,52 @@ class ConsecutiveRule(Rule):
             else:
                 cur_pattern = self.regex_pattern.get(a, None)
                 if cur_pattern:
-                    _pattern += cur_pattern
-                    _pattern += f"{{{n}}}" if (n > 1) else ""
+                    pattern += cur_pattern
+                    pattern += f"{{{n}}}" if (n > 1) else ""
                 n = 1
-        self.pattern = re.compile(_pattern)
-        self.point = point
+        self._pattern = re.compile(pattern)
+        self._seq = seq
+        self.prompt = None
 
-    def calc_point(self, word):
-        return len(self.pattern.findall(word)) * self.point
+    def calc_score(self, word):
+        return len(self._pattern.findall(word)) * self.point
+
+    def generate_prompt(self):
+        if self.prompt is not None:
+            return self.prompt
+
+        prompt = None
+        if len(self._seq) == 1 and self._seq[0] in {'c', 'v'}:
+            prompt = f"every {'consonant' if self._seq == 'c' else 'vowel'}"
+        elif self._seq == "cc":
+            prompt = f"every pair of consecutive consonant"
+        elif self._seq == "vv":
+            prompt = f"every pair of consecutive vowel"
+        elif self._seq == "vc":
+            prompt = f"every consonant right after a vowel"
+        elif self._seq == "cv":
+            prompt = f"every vowel right after a consonant"
+        elif len(set(self._seq)) == 1 and self._seq[0] in {'c', 'v'}:
+            prompt = f"every {len(self._seq)} consecutive {'consonant' if self._seq == 'c' else 'vowel'}s"
+
+        if prompt is None:
+            raise NotImplementedError(f"[{self.__class__}] Not implemented for pattern: '{self._seq}'")
+        else:
+            self.prompt = self.point_wrapper(prompt, randint=0)
+        return self.prompt
 
 
 #%%
-class LengthRule(Rule):
+class LengthScoring(Scoring):
     def __init__(self, point=1, lt=None, gt=None, eq=None, ne=None):
-        super().__init__()
+        super().__init__(point)
         self.point = point
         self.lt, self.gt = lt or np.inf, gt or -np.inf
         self.eq = eq if (lt is None) and (gt is None) else None
         self.ne = ne
+        self.prompt = None
 
-    def calc_point(self, word):
+    def calc_score(self, word):
         n = len(word)
         if not (self.gt < n < self.lt):
             return 0
@@ -116,36 +149,99 @@ class LengthRule(Rule):
             return 0
         return self.point
 
+    def generate_prompt(self):
+        if self.prompt is not None:
+            return self.prompt
+
+        prompt = None
+        if self.lt < np.inf and not (self.gt > -np.inf) and self.ne is None:
+            prompt = f"word less than {self.lt} characters"
+
+        # print(self.lt, self.gt, self.eq, self.ne)
+        # print(self.lt < np.inf, self.gt > -np.inf)
+
+        if prompt is None:
+            raise NotImplementedError(f"[{self.__class__}] Not implemented for current condition ()")
+        else:
+            self.prompt = self.point_wrapper(prompt, randint=0)
+        return self.prompt
+
 
 #%%
-class AffixRule(Rule):
+class AffixScoring(Scoring):
     def __init__(self, point=1, prefix=None, suffix=None):
-        super().__init__()
+        super().__init__(point)
         self.point = point
+        self.prefix_txt, self.suffix_txt = prefix, suffix
         self.prefix = None if prefix is None else re.compile(f"^{prefix}")
         self.suffix = None if suffix is None else re.compile(f"{suffix}$")
+        self.prompt = None
 
-    def calc_point(self, word):
+    def calc_score(self, word):
         if self.prefix is not None and self.prefix.search(word) is None:
             return 0
         if self.suffix is not None and self.suffix.search(word) is None:
             return 0
         return self.point
 
+    def generate_prompt(self):
+        if self.prompt is not None:
+            return self.prompt
+
+        prompt = None
+        if self.prefix is not None and self.suffix is None:
+            prompt = f"word starts with {self.prefix_txt}"
+        elif self.prefix is None and self.suffix is not None:
+            prompt = f"word ends with {self.suffix_txt}"
+
+        if prompt is None:
+            raise NotImplementedError(f"[{self.__class__}] Not implemented for current condition ({repr(self)})")
+        else:
+            self.prompt = self.point_wrapper(prompt, randint=0)
+        return self.prompt
+
 
 #%%
-class InfixRule(Rule):
+# - every pair of consecutive consonant has 5 points
+# - additional 1 point if there exists exactly 1 'g'
+# - word less than 5 characters gets extra 10 points
+# - word starts with 'gen' gets additional 100 points
+# - word ends with 'ta' gets negative 1000 points
+
+
+#%%
+class InfixScoring(Scoring):
     def __init__(self, point=1, infix=None, n=None):
-        super().__init__()
+        super().__init__(point)
         self.point = point
+        self.infix = infix
         self.pattern = re.compile(infix)
         self.n = n
+        self.prompt = None
 
-    def calc_point(self, word):
+    def calc_score(self, word):
         if self.n is None:
             return 0 if self.pattern.search(word) is None else self.point
         else:
-            return len(self.pattern.findall(word)) * self.point
+            return (len(self.pattern.findall(word)) == self.n) * self.point
+
+    def generate_prompt(self):
+        if self.prompt is not None:
+            return self.prompt
+
+        prompt = None
+        assert self.infix is not None, "owowo"
+        if self.n is None:
+            prompt = f"there exists '{self.infix}' in the word"
+        else:
+            prompt = f"there exists exactly {self.n} '{self.infix}' in the word"
+
+        if prompt is None:
+            raise NotImplementedError(f"[{self.__class__}] Not implemented for current condition ({repr(self)})")
+        else:
+            self.prompt = self.point_wrapper(prompt, randint=1)
+        return self.prompt
+
 
 
 #%%
@@ -158,7 +254,7 @@ class TheGame:
     def calc_point(self, word):
         ret = 0
         for rule in self.rules:
-            ret += rule.calc_point(word)
+            ret += rule.calc_score(word)
         return ret
 
     def get_point(self, word):
@@ -189,14 +285,19 @@ if __name__ == '__main__':
 
     thegame = TheGame(
         rules={
-            ConsecutiveRule(point=5, seq="cc"),
-            InfixRule(point=1, infix="g", n=1),
-            LengthRule(point=10, lt=5),
-            AffixRule(point=100, prefix="gen"),
-            AffixRule(point=-1000, suffix="ta"),
+            ConsecutiveScoring(point=5, seq="cc"),
+            InfixScoring(point=1, infix="g", n=1),
+            LengthScoring(point=10, lt=5),
+            AffixScoring(point=100, prefix="gen"),
+            AffixScoring(point=-1000, suffix="ta"),
         },
         words=set(words),
     )
+
+    print("Rule Prompt List:")
+    for rule in thegame.rules:
+        print(f" - {rule.generate_prompt()}")
+    print("==========")
 
     def calc_point(word, verbose=False):
         cnt = 0
